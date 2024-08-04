38d2065812f0555806174713ca42e478206042e1
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-08-05 00:32:06 +0600

    loading page added

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='h-screen'>
+            PageLoDing ......
+        </div>
+    );
+};
+
+export default loading;
